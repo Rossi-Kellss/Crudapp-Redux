34f77c0272f560a97bd25fe3deb50da92c2cf277
Rossi-Kellss <rossi.codingspace@gmail.com>
2023-07-04 22:38:35 +0545

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
 // import './App.css'
+import Create from './components/Create'
 import Navbar from './components/Navbar'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
     <Navbar/>
+    <Routes>
+      <Route exact path='/' element={<Create/>}/>
+    </Routes>
+    </BrowserRouter>
+    
     </>
   )
 }
--- src/features/userDetail.js
+++ src/features/userDetail.js
@@ -1,4 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const createUser=createAsyncThunk("createUser",async (users,{rejectWithValue})=>{
+    const response=await fetch("https://64a41173c3b509573b5700e2.mockapi.io/CRUD",{
+        method:'POST',
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(users)
+    })
+
+    try {
+        const result=response.json();
+        return result
+    } catch (error) {
+        return(rejectWithValue(error));
+        
+    }
+})
 
 export const userDetails=createSlice({
     name:"user",
@@ -6,6 +24,21 @@ export const userDetails=createSlice({
         users:[],
         loading:false,
         error:null
+    },
+    extraReducers:{
+        [createUser.pending]:(state,action)=>{
+            state.loading=false;
+            state.users.push(action.payload);
+        },
+        [createUser.pending]:(state)=>{
+            state.loading=true;
+
+        },
+        [createUser.rejected]:(state,action)=>{
+            state.loading=false;
+            state.users=action.payload;
+        },
+
     }
 
 })
